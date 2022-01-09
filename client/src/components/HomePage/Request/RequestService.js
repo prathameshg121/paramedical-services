288e40ba60168fb6902cd68a374c81a6da938d4f
theshivv <shivamdarasingpawar@gmail.com>
2022-01-09 17:34:01 +0530

    added district options in dropdown

--- client/src/components/HomePage/Request/RequestService.js
+++ client/src/components/HomePage/Request/RequestService.js
@@ -197,11 +197,45 @@ let year = newDate.getFullYear();
         name="state"
       >
         <option selected>All</option>
-        <option value="Uttar Pradesh">Uttar Pradesh</option>
-        <option value="Tamil Nadu">Tamil Nadu</option>
-        <option value="Rajasthan">Rajasthan</option>
-        <option value="Maharashtra">Maharashtra</option>
-        <option value="Karnataka">Karnataka</option>
+        <option value="Ahmednagar">Ahmednagar</option>
+        <option value="Akola">Akola</option>
+        <option value="Amravati">Amravati</option>
+        <option value="Aurangabad">Aurangabad</option>
+        <option value="Beed">Beed</option>
+        <option value="Bhandara">Bhandara</option>
+        <option value="Buldhana">Buldhana</option>
+        <option value="Chandrapur">Chandrapur</option>
+        <option value="Dhule">Dhule</option>
+        <option value="Gadchiroli">Gadchiroli</option>
+        <option value="Gondia">Gondia</option>
+        <option value="Hingoli">Hingoli</option>
+        <option value="Jalgaon">Jalgaon</option>
+        <option value="Jalna">Jalna</option>
+        <option value="Kolhapur">Kolhapur</option>
+        <option value="Latur">Latur</option>
+        <option value="Mumbai-City">Mumbai-City</option>
+        <option value="Mumbai-Suburban">Mumbai-Suburban</option>
+        <option value="Nagpur">Nagpur</option>
+        <option value="Nanded">Nanded</option>
+        <option value="Nandurbar">Nandurbar</option>
+        <option value="Osmanabad">Osmanabad</option>
+        <option value="Palghar">Palghar</option>
+        <option value="Parbhani">Parbhani</option>
+        <option value="Pune">Pune</option>
+        <option value="Raigad">Raigad</option>
+        <option value="Ratnagiri">Ratnagiri</option>
+        <option value="Sangli">Sangli</option>
+        <option value="Satara">Satara</option>
+        <option value="Sindhudurg">Sindhudurg</option>
+        <option value="Solapur">Solapur</option>
+        <option value="Thane">Thane</option>
+        <option value="Wardha">Wardha</option>
+        <option value="Washim">Washim</option>
+        <option value="Yavatmal">Yavatmal</option>
+
+
+        {/* */}
+        
       </select>
        </div>
 
